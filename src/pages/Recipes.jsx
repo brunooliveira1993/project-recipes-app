8dfa37ad9f2f12c6254fca34b9b97589675eca0c
Pedro Costa <pedrohrc@outlook.com.br>
2022-09-29 17:11:17 -0300

    correção de lint

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -8,7 +8,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import CardMain from '../components/CardMain';
 import { fetchRecipes } from '../services';
-import './Recipes.css'
+import './Recipes.css';
 
 function Recipes() {
   const {
